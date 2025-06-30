fa0515be2473fa4634026ae3f675d6413fd5114f
Patna-an Jr. <jdp5378@students.uc-bcf.edu.ph>
2025-06-30 17:19:38 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { 
   const form = document.getElementById("contactForm");
   if (form) {
     form.addEventListener("submit", function (e) {
